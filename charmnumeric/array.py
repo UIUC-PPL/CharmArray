b990b299177c5a89fbe5db3b8d93a85f9b02303a
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-10-17 02:07:46 +0530

    fix deletion of validated arrays

--- charmnumeric/array.py
+++ charmnumeric/array.py
@@ -269,22 +269,18 @@ class ndarray:
             self.command_buffer.plot_graph()
         if self.valid:
             return
-        validated_arrays = {self.name : self}
+        validated_arrays = []
         cmd = self.command_buffer.get_command(validated_arrays, self.ndim, self.shape, is_scalar=self.is_scalar)
-        reply_size = 0
-        for name, arr in validated_arrays.items():
-            reply_size += 8 + 8 * arr.ndim
         if not debug:
             cmd = to_bytes(deletion_buffer_size, 'I') + deletion_buffer + cmd
             cmd = to_bytes(get_epoch(), 'i') + to_bytes(len(cmd), 'I') + cmd
             send_command_async(Handlers.operation_handler, cmd)
             deletion_buffer = b''
             deletion_buffer_size = 0
-            for i in range(len(validated_arrays)):
-                arr = validated_arrays[name]
+            for arr in validated_arrays:
                 arr.validate()
         else:
-            for name, arr in validated_arrays.items():
+            for arr in validated_arrays:
                 arr.validate()
         self.validate()
 
@@ -307,7 +303,7 @@ class ndarray:
 
     def validate(self):
         self.valid = True
-        self.command_buffer = ASTNode(self.name, 0, [self])
+        self.command_buffer = ASTNode(self.name, 0, [weakref.proxy(self)])
 
     def copy(self):
         res = get_name()
