d5bd4d1a678d5590c9512e3676e3b3440590081d
Sh0g0-1758 <shouryagoel10000@gmail.com>
2025-10-19 01:46:26 +0530

    nit

--- examples/charm_fuse.py
+++ examples/charm_fuse.py
@@ -4,12 +4,12 @@ from charmnumeric.ccs import enable_debug
 import charmnumeric.linalg as lg
 import numpy as np
 import time
-set_max_depth(10)
+set_max_depth(float('inf'))
 
 @charm_fuse
 def f():
-    v = ndarray(1, 1e5, np.float64, init_value=-20)
-    b = ndarray(1, 1e5, np.float64, init_value=10)
+    v = ndarray(1, 1e2, np.float64, init_value=-20)
+    b = ndarray(1, 1e2, np.float64, init_value=10)
 
     g1 =  v.abs().scale(2).scale(2).add_constant(29) + b + 32
     g2 = b.log(2).exp()
@@ -17,8 +17,8 @@ def f():
     return d.get()
 
 def g():
-    v = ndarray(1, 1e5, np.float64, init_value=-20)
-    b = ndarray(1, 1e5, np.float64, init_value=10)
+    v = ndarray(1, 1e2, np.float64, init_value=-20)
+    b = ndarray(1, 1e2, np.float64, init_value=10)
 
     g1 =  v.abs().scale(2).scale(2).add_constant(29) + b + 32
     g2 = b.log(2).exp()
@@ -27,18 +27,18 @@ def g():
 
 if __name__ == '__main__':
     connect("127.0.0.1", 10000)
-    s = f()
+    # s = f()
     start = time.time()
-    for(i) in range(100):
+    for(i) in range(1000):
         s = f()
     end = time.time()
     print(s)
     print("Time taken(multi line fused): ", end - start)
 
-    s = g()
-    start = time.time()
-    for(i) in range(100):
-        s = g()
-    end = time.time()
-    print(s)
-    print("Time taken(multi line unfused): ", end - start)
+    # k = g()
+    # start = time.time()
+    # for(i) in range(100):
+    #     k = g()
+    # end = time.time()
+    # print(k)
+    # print("Time taken(multi line unfused): ", end - start)
--- src/ast.hpp
+++ src/ast.hpp
@@ -17,17 +17,16 @@ std::unordered_map<ct_name_t, ct_array_t> symbol_table;
 inline static void insert(ct_name_t name, ct_array_t arr) {
   CkPrintf("Created array %" PRIu64 " on server\n", name);
   symbol_table[name] = std::move(arr);
-  { 
-    ckout<<"Available Symbols"<<endl;
-      for(const auto& it : symbol_table){
-        ckout<<it.first<<" ";
-      }
-      ckout<<endl;
-  }
 }
 
-inline static void remove(ct_name_t name) noexcept { 
-  ckout<<"removing " << name<<endl;
+inline static void remove(ct_name_t name) noexcept {
+  { 
+    ckout<< "Before deletion, the available symbols were: ";
+    for (const auto& it : symbol_table)
+      ckout<< it.first << " ";
+    ckout << endl;
+    ckout<< "and I have been tasked to remove " << name << endl;
+  }
   symbol_table.erase(name); 
 }
 
@@ -35,13 +34,7 @@ static ct_array_t &lookup(ct_name_t name) {
   auto find = symbol_table.find(name);
   CkPrintf("Looking up array %" PRIu64 " on server\n", name);
   if (find == std::end(symbol_table))
-    { ckout<<"Available Symbols"<<endl;
-      for(const auto& it : symbol_table){
-        ckout<<it.first<<" ";
-      }
-      ckout<<endl;
-      
-      CmiAbort("Symbol%" PRIu64 "not found", name);}
+    CmiAbort("Symbol%" PRIu64 "not found", name);
   return find->second;
 }
 
